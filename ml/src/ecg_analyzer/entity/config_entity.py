b2a65d0be1c85f3323761d02c6fca85f4474ae94
Jeet Biswas <jeetbiswas816@gmail.com>
2026-07-06 22:08:37 +0530

    updated data validation part

--- ml/src/ecg_analyzer/entity/config_entity.py
+++ ml/src/ecg_analyzer/entity/config_entity.py
@@ -10,3 +10,14 @@ class DataIngestionConfig:
 
     train_file_name: str
     test_file_name: str
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    raw_data_dir: Path
+    train_file_name: str
+    test_file_name: str
+    status_file: Path
+    expected_number_of_columns: int
+    target_column_index: int
+    allowed_target_classes: list[int]
